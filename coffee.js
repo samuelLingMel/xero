f0c8aa4f352ce663190e8c2a8020adbfe79f9778
Samuel <lingofthesam@gmail.com>
2020-08-01 17:36:53 +1000

    coffee test and function finished

--- coffee.js
+++ coffee.js
@@ -1,5 +1,35 @@
-const coffee = function(array) {
-  return array
+
+const coffee = function(input) {
+  let offices = {};
+  
+  input.forEach( ele => {
+    const nameMonthAmount = ele.split(',');
+    if (offices[`${nameMonthAmount[0]}`]) {
+      offices[`${nameMonthAmount[0]}`].cupsOfCoffee += Number(nameMonthAmount[2]);  
+      offices[`${nameMonthAmount[0]}`].months += 1;  
+    } else {
+      offices[`${nameMonthAmount[0]}`] = {};  
+      offices[`${nameMonthAmount[0]}`].name = nameMonthAmount[0];  
+      offices[`${nameMonthAmount[0]}`].cupsOfCoffee = Number(nameMonthAmount[2]);  
+      offices[`${nameMonthAmount[0]}`].months = 1;  
+    }
+  })
+
+
+  const formatResponse = function(listOfObjects) {
+    const formattedOffices = [];
+
+    Object.keys(listOfObjects).forEach( officeListName => {
+        formattedOffices.push(
+        `${listOfObjects[officeListName].name},${listOfObjects[officeListName].cupsOfCoffee},${Math.floor((listOfObjects[officeListName].cupsOfCoffee) / listOfObjects[officeListName].months)}`);
+    })
+    
+    return formattedOffices;
+  }
+  return formatResponse(offices);
 }
 
-module.exports = coffee
+module.exports = coffee;
+
+
+
